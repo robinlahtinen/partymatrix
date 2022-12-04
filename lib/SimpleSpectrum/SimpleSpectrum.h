a3870c92bc450d263521e08b1709db429a28dad0
Robin Lahtinen <robin.lahtinen@gmail.com>
2022-12-04 21:33:53 +0200

    feat(SimpleSpectrum): added base for SimpleSpectrum visualization method
    Signed-off-by: Robin Lahtinen <robin.lahtinen@gmail.com>

---
+++ lib/SimpleSpectrum/SimpleSpectrum.h
@@ -0,0 +1,10 @@
+//
+// Created by Robin on 4.12.2022.
+//
+
+#ifndef PARTYMATRIX_SIMPLESPECTRUM_H
+#define PARTYMATRIX_SIMPLESPECTRUM_H
+
+void doVisualizeSimpleSpectrum(int strength);
+
+#endif //PARTYMATRIX_SIMPLESPECTRUM_H
